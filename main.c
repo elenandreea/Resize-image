f86bae9e919f165cb00fcd69b9c4b4f56aa0cd8f
elenandreea <36417601+elenandreea@users.noreply.github.com>
2018-11-05 15:24:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//DO NOT MODIFY THIS FILE
 extern int num_threads;
 extern int resize_factor;
 int main(int argc, char * argv[]) {
@@ -16,27 +15,27 @@ int main(int argc, char * argv[]) {
 		printf("Incorrect number of arguments\n");
 		exit(-1);
 	}
-//DO NOT MODIFY THIS FILE
+
 	resize_factor = atoi(argv[3]);
 	num_threads = atoi(argv[4]);
-//DO NOT MODIFY THIS FILE
+
 	image input;
 	image output;
-//DO NOT MODIFY THIS FILE
+
 	readInput(argv[1], &input);
-//DO NOT MODIFY THIS FILE
+
 	struct timespec start, finish;
 	double elapsed;
-//DO NOT MODIFY THIS FILE
+
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	resize(&input, &output);
 	clock_gettime(CLOCK_MONOTONIC, &finish);
-//DO NOT MODIFY THIS FILE
+
 	elapsed = (finish.tv_sec - start.tv_sec);
 	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-//DO NOT MODIFY THIS FILE
+
 	printf("%lf\n", elapsed);
-//DO NOT MODIFY THIS FILE
+
 	writeData(argv[2], &output);
 	return 0;
 }
